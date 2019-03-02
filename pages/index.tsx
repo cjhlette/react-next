b97feb224ef6667da61945169e05f1009c6f2ed1
cjhlette <cjh514@gmail.com>
2019-03-02 18:50:31 +0900

    koa server

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import * as styles from "../styles/main.scss";
 import { Layout } from "../components/Layout";
 
 export default () => (
-  <Layout title="HOME2">
+  <Layout title="HOME">
     <h1 className={styles.pageTitle}>Next.js example</h1>
     <ul className="list">
       <li>Typescript</li>
--- server.js
+++ server.js
@@ -1,24 +1,58 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+// const { createServer } = require("http");
+// const { parse } = require("url");
+// const next = require("next");
+// const compression = require("compression");
+//
+// const port = parseInt(process.env.PORT, 10) || 3000;
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+//
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     const { pathname, query } = parsedUrl;
+//
+//     if (pathname.length > 1 && pathname.slice(-1) === "/") {
+//       app.render(req, res, pathname.slice(0, -1), query);
+//     } else {
+//       handle(req, res, parsedUrl);
+//     }
+//   }).listen(port, err => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const Koa = require('koa')
+const next = require('next')
+const Router = require('koa-router')
+const compression = require('compression')
+const koaConnect = require('koa-connect')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
+  const server = new Koa()
+  const router = new Router()
 
-    if (pathname.length > 1 && pathname.slice(-1) === "/") {
-      app.render(req, res, pathname.slice(0, -1), query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+  server.use(koaConnect(compression()))
+
+  router.get('*', async ctx => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
+  })
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200
+    await next()
+  })
+
+  server.use(router.routes())
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
