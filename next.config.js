e8247c4b56b3fd737e50787b58f40c99232af488
cjhlette <cjh514@gmail.com>
2019-03-03 22:11:53 +0900

    polyfill

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const withSass = require("@zeit/next-sass");
 
 module.exports = withTypescript(
   withSass({
-    webpack(config, options) {
+    webpack: (config, options) => {
+      // Unshift polyfills in main entrypoint.
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
+        if (entries['main.js']) {
+          entries['main.js'].unshift('./static/js/polyfills.js'); // <- polyfill here
+        }
+        return entries;
+      };
+
       return config;
     },
     cssModules: true,
